aa59dc1cc33d8b16ef67add27557dbd02421fb8b
Julie Sunny Mathew <jsunnymathew@unomaha.edu>
2020-05-09 17:32:50 -0500

    Added GITHUB token

--- greetingScript.sh
+++ greetingScript.sh
@@ -2,12 +2,11 @@
 set -eo pipefail
 greeting() {
  local _GITHUB_USER="${1}"
- echo $_GITHUB_USER
  local _str="Hello, $_GITHUB_USER !"
  echo $_str
  return $?
 }
-greeting "StrategicFS"
+greeting ${{ secrets.GITHUB_TOKEN }}
 RET=$?
 if true || [ ${RET} -ne 0 ] ; then
  echo 'ERR: Failed to give greeting' > /dev/stderr
