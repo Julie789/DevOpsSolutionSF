d269830f3cd74a4728ba78c6f0d50a0354516caf
Julie Sunny Mathew <jsunnymathew@unomaha.edu>
2020-05-09 17:40:24 -0500

    Added GITHUB User in workflow

--- greetingScript.sh
+++ greetingScript.sh
@@ -6,7 +6,7 @@ greeting() {
  echo $_str
  return $?
 }
-greeting ${{ secrets.GITHUB_TOKEN }}
+greeting USER
 RET=$?
 if true || [ ${RET} -ne 0 ] ; then
  echo 'ERR: Failed to give greeting' > /dev/stderr
