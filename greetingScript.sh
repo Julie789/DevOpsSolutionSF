50fcbdd16a4181b3156b0314daabb253854c0bf3
Julie Sunny Mathew <jsunnymathew@unomaha.edu>
2020-05-10 17:53:27 -0500

    Updated witth default environment variables

--- greetingScript.sh
+++ greetingScript.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 set -eo pipefail
 greeting() {
-  local _job="${2}"
-  for key in "${!_job[@]}"; do
-   echo "key=$key, value=${_job[$key]}"
-   done
  local _GITHUB_USER="${1}"
  local _str="Hello, $_GITHUB_USER !"
  echo $_str
  return $?
 }
-greeting "$USER" "$job"
+greeting "$USER"
 RET=$?
 #if true || [ ${RET} -ne 0 ] ; then
 if [ ${RET} -ne 0 ] ; then    #Removed by default true check, it will always go to if block
