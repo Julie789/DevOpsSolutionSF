2f840a2a31a313c2aa09022f4805d1e35ddbc6e7
Julie Sunny Mathew <42555719+Julie789@users.noreply.github.com>
2020-05-11 01:38:50 -0500

    Develop (#7)
    * Updated Script to show Strategic Financial
    
    * Updated build error
    
    * Updated to show GITHUB USER
    
    * Updated to show GITHUB USER
    
    * Updated to show GITHUB USER
    
    * Updated to show GITHUB USER
    
    * Added GITHUB token
    
    * Added GITHUB User in workflow
    
    * Added $ before using the variable
    
    * Removed the extra true check in if statement
    
    * echo RET
    
    * removed extra echo
    
    * removed true check in if condition
    
    * Feature/commitsdetails (#6)
    
    * Fetching job run details in workflow
    
    * Fetching job run details in workflow
    
    * Adding more job run details in workflow
    
    * Adding more job run details in workflow
    
    * Adding more job run details in workflow
    
    * Adding more job run details in workflow
    
    * Adding more job run details in workflow
    
    * Fetching the id of the job
    
    * added script to show job details
    
    * added full permission to the jobDetails script
    
    * added full permission to the jobDetails script
    
    * error in building new script
    
    * removed the call of new script
    
    * added the status
    
    * added the id
    
    * added the id object
    
    * Check the steps id
    
    * Check the run id
    
    * Check the job object
    
    * Checking the job object
    
    * Checking the job object
    
    * Removed git log command
    
    * Updated witth default environment variables
    
    * Updated witth github objects
    
    * Added new job with run information
    
    * Added job details in script file
    
    * Added commits object
    
    * Added pull requests in commits object
    
    * Removed !
    
    * Updated the workflow name and few changes in script
    
    * Rectified build error

---
+++ jobDetails.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -eo pipefail
+jobInfo() {
+ local _commits="${1}", _commits_url="${2}", _workflow="${3}", _workspace="${4}"
+ local _commit_str="The number of commits committed to $_commits_url are $_commits"
+ local _workspace_str="The workflow $_workflow tested the initial checks in workspace $_workspace"
+ echo $_commit_str
+ echo $_workspace_str
+ return $?
+}
+jobInfo "$commits" "$commits_url" "$workflow" "$workspace"
+RET=$?
+if [ ${RET} -ne 0 ] ; then
+ echo 'ERR: Failed to give job details' > /dev/stderr
+else
+ echo 'DEBUG: Job Details displayed' > /dev/stderr
+fi
