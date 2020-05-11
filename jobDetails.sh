1df9e9d482358ba643db3980d63e7b3d52907f6f
Julie Sunny Mathew <jsunnymathew@unomaha.edu>
2020-05-10 23:20:08 -0500

    Removed !

--- jobDetails.sh
+++ jobDetails.sh
@@ -2,7 +2,7 @@
 set -eo pipefail
 jobInfo() {
  local _commits="${1}", _commits_url="${2}", _workflow="${3}", _workspace="${4}"
- local _commit_str="The number of commits committed to $_commits_url are $_commits !"
+ local _commit_str="The number of commits committed to $_commits_url are $_commits"
  local _workspace_str="The workflow $_workflow tested the initial checks in workspace $_workspace"
  echo $_commit_str
  echo $_workspace_str
